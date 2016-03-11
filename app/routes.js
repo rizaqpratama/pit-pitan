9bd24638966ee333af820b10c407af417e3e43e5
Rizaq Pratama <rizaqpratama@gmail.com>
2016-03-11 16:00:22 +0700

    initial

--- app/routes.js
+++ app/routes.js
@@ -24,4 +24,55 @@ module.exports= function(app){
       res.json(req.body);
     });
   });
+  app.post('/query/',function(req,res){
+    var lat = req.body.latitude;
+    var long = req.body.longitude;
+    var distance= req.body.distance;
+    var male = req.body.male;
+    var female = req.body.female;
+    var other = req.body.other;
+    var minAge= req.body.minAge;
+    var maxAge= req.body.maxAge;
+    var favlang= req.body.favlang;
+    var reqVerified= req.body.reqVerified;
+
+
+
+    var query = User.find({});
+
+
+    if(distance){
+      query = query.where('location').near({center: {type: 'Point', coordinates: [long,lat]},
+      maxDistance : distance * 1609.34, spherical: true
+    });
+    }
+
+    if(male || female || other){
+      query.or([{'gender':male},{'gender':female},{'gender': other}]);
+    }
+
+    if(minAge){
+      query =query.where('age').gte(minAge);
+    }
+
+    if(maxAge){
+      query=query.where('age').lte(maxAge);
+    }
+
+    if(favlang){
+      query=query.where('favlang').equals(favlang);
+    }
+
+    if(reqVerified){
+      query = query.where('htmlverified').equals("Hore!!!! Verified user!");
+    }
+
+    query.exec(function(err,users){
+      if(err)
+        res.send(err);
+
+      res.json(users);
+    });
+
+  });
 };
--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -17,6 +17,16 @@ addCtrl.controller('addCtrl',function($scope,$http,$rootScope,geolocation,gservi
     });
   });
 
+  geolocation.getLocation().then(function(data){
+    coords= {lat:data.coords.latitude,long: data.coords.longitude};
+
+    $scope.formData.longitude= parseFloat(coords.long).toFixed(3);
+    $scope.formData.latitude= parseFloat(coords.lat).toFixed(3);
+
+    $scope.formData.htmlverified="Hore!!!! Verified user!";
+    gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
+  });
+
 
   $scope.createUser= function(){
 
